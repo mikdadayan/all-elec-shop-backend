5ecf9bcd67c91051def9e6e315c6263c6aef4157
Mikayel <dadayan.mikayel@gmail.com>
2022-02-07 00:12:56 +0400

    feat: seeder script, fetch products from DB

--- src/api/controllers/products.ts
+++ src/api/controllers/products.ts
@@ -1,11 +1,42 @@
 import { RequestHandler } from 'express';
-import products from '../../data/products';
+import Product from '../../models/Product';
 
-export const getProducts: RequestHandler = (_req, res, _next) => {
-  res.status(200).json(products);
+interface ProductType {
+  _id: string;
+  name: string;
+  image: string;
+  description: string;
+  brand: string;
+  category: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+}
+
+export const getProducts: RequestHandler = async (_req, res, _next) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    let err = error as Error;
+    console.log(`Error: ${err.message}`.red);
+    // throw new Error(`Something went wrong`);
+  }
 };
 
-export const getProduct: RequestHandler = (req, res, _next) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+export const getProduct: RequestHandler = async (req, res, _next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Product not found' });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    let err = error as Error;
+    console.log(`Error: ${err.message}`.red);
+    // throw new Error(`Something went wrong`);
+  }
 };
